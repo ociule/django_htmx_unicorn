182ea5810c86864f0507220082b2f2fc9c5654cf
Ovidiu Ciule <ovidiu.ciule@gmail.com>
2022-11-21 16:46:39 +0100

    fly.io env

--- django_htmx_unicorn/settings.py
+++ django_htmx_unicorn/settings.py
@@ -12,20 +12,25 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import environ
+
+env = environ.Env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+environ.Env.read_env(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ih1fka3gkg2(snvg$c72(1*9!jsa-%br$njgb!)_08q!99z5j)'
+SECRET_KEY = SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
